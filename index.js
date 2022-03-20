e1afabfb0bc997a6bc25e40d31feb394879fe2a9
Tuuli Toivanen-Gripentrog <tuulitg@Tuuli-MacBook-Pro.local>
2022-03-20 20:15:21 +0200

    Tehtävä 13.1 tehty

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-const express = require('express')
-const app = express()
+require('dotenv').config()
+const { Sequelize } = require('sequelize')
 
-app.use(express.json())
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-01-10T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-01-10T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-01-10T19:20:14.298Z",
-      important: true
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-  ]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  },
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection has been established successfully.')
+    sequelize.close()
+  } catch (error) {
+    console.error('Unable to connect to the database:', error)
+  }
+}
 
-const PORT = 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+main()
