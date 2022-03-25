11c0a093f77bcf8ee9a403e11e8408e53a4cf117
Tuuli Toivanen-Gripentrog <tuulitg@Tuuli-MacBook-Pro.local>
2022-03-25 13:00:19 +0200

    Tehtävä 13.12

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 require('express-async-errors')
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
 
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
@@ -9,7 +9,11 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('', async (req, res) => {
-    const blogs = await Blog.findAll()
+    const blogs = await Blog.findAll({
+        include: {
+            model: User
+        }
+    })
     res.json(blogs)
 })
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,23 @@
 const router = require('express').Router()
 require('express-async-errors')
-const { User } = require('../models')
+const { User, Blog } = require('../models')
 
 const userFinder = async (req, res, next) => {
     req.user = await User.findAll({
-        where: {username: req.params.username}
+        where: {username: req.params.username},
+        include: {
+            model: Blog
+        }
     })
     next()
 }
 
 router.get('/', async (req, res) => {
-  const users = await User.findAll()
+  const users = await User.findAll({
+      include: {
+          model: Blog
+      }
+  })
   res.json(users)
 })
 
