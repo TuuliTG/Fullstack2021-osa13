853fc7e8336c1e7ab51df95b438638aa0592e99a
Tuuli Toivanen-Gripentrog <tuulitg@Tuuli-MacBook-Pro.local>
2022-03-25 10:11:47 +0200

    Tehtävä 13.4

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, DataTypes, Model } = require('sequelize')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -12,24 +14,85 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 })
 
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull:false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
 const main = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connection has been established successfully.')
-    //sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error)
   }
 }
 
 app.get('/api/blogs', async (req, res) => {
-    const blogs = await sequelize.query("SELECT * FROM blogs", { type: QueryTypes.SELECT })
+    try{
+    const blogs = await Blog.findAll()
     res.json(blogs)
-  })
+    } catch(error) {
+        console.log('error in get')
+        return res.status(400).json({ error })
+    }
+})
+
+app.post('/api/blogs', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body)
+        res.json(blog)
+    } catch(error) {
+        console.log('error in post')
+        return res.status(400).json({ error })
+    }
+    
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {    
+    const id = req.params.id
+    try {
+        await Blog.destroy({
+            where: {
+                id: id
+            }
+        })
+        res.status(204).end()
+    } catch(error) {
+        console.log('error in delete')
+        return res.status(400).json({ error })
+    }
+})
   
-  const PORT = process.env.PORT || 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
 
 main()
